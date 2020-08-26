f7da95ab668113c0f792eb8b5e3c15a289b7f833
Denis <d-h11@ukr.net>
2020-08-26 13:05:52 +0300

    fix header & add modal

---
+++ src/js/scripts.js
@@ -0,0 +1,9 @@
+// import modaal from '~../../node_modules/modaal/source/js/modaal.js';
+
+$(document).ready(function () {
+    console.log('ok');
+
+    $('.inline').modaal();
+});
+
+console.log('hello');
